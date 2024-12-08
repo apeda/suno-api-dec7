74619d88e0cacb6b07219a3f92de046a3f92b238
Sergey Guk <eventpromoby@gmail.com>
2024-12-08 18:22:41 +0100

    Get Implementation

---
+++ src/app/api/get_implementation/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse, NextRequest } from "next/server";
+import { corsHeaders } from "@/lib/utils";
+export const dynamic = "force-dynamic";
+
+export async function GET(req: NextRequest) {
+  if (req.method === 'GET') {
+    try {
+      return new NextResponse(JSON.stringify({
+        type: process.env.IMPLEMENTATION_TYPE
+      }), {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+          ...corsHeaders
+        }
+      });
+    } catch (error: any) {
+      return new NextResponse(JSON.stringify({ error: 'Internal server error: ' + error.message }), {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+          ...corsHeaders
+        }
+      });
+    }
+  } else {
+    return new NextResponse('Method Not Allowed', {
+      headers: {
+        Allow: 'GET',
+        ...corsHeaders
+      },
+      status: 405
+    });
+  }
+}
+
+
+export async function OPTIONS(request: Request) {
+  return new Response(null, {
+    status: 200,
+    headers: corsHeaders
+  });
+}
